bc0854a570a986e0b5c85cfea7f50d6c9f4b83d0
krutetskiy1488 <kingsemyn@gmail.com>
2021-03-18 16:30:49 +0300

    label constants on the screen

---
+++ Labaratory/Assets/Scripts/ShowConstants.cs
@@ -0,0 +1,20 @@
+﻿using static AssemblyCSharp.Assets.Scripts.Constants;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowConstants : MonoBehaviour
+{
+    public Text TextConstants;
+
+    void Start()
+    {
+        var str =
+            $"Глубина забивки трубы: {PipeCloggingDepth[1]}\n" +
+            $"Диаметр трубы: {PipeDiameter[1]}\n" +
+            $"Климатический коэффициент: {ClimateCoefficient[1]}\n" +
+            $"Необходимое сопротивление: {RequiredResistance[1]}\n" +
+            $"Форма заземления: {GroundConnectionForm[1]}\n";
+
+        TextConstants.text = str;
+    }
+}
